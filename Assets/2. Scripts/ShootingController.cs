b0e22ae40530ab95f4cc93718d7eadfe89da4e6d
MK <simon365@naver.com>
2023-06-05 18:06:08 +0900

    Player Movement with WASD

--- Assets/2. Scripts/ShootingController.cs
+++ Assets/2. Scripts/ShootingController.cs
@@ -16,15 +16,53 @@ public class ShootingController : MonoBehaviour
 
     private float moveX;
     private float moveY;
+    private bool isDead = false;
 
 
     void Start()
     {
-        
+        PlayerState = PlayerStates.Idle;
     }
 
     void Update()
     {
-        
+        if (isDead)
+            return;
+
+        PlayerState = PlayerStates.Idle;
+        TryMove();
+        TryRoll();
+
+        switch (PlayerState)
+        {
+            case PlayerStates.Idle:
+                break;
+            case PlayerStates.Move:
+                Move();
+                break;
+            case PlayerStates.Dead:
+                break;
+        }
+    }
+
+    private void TryMove()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            return;
+
+        PlayerState = PlayerStates.Move;
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void Move()
+    {
+        Vector3 moveVelocity = new Vector3(moveX, moveY, 0).normalized * moveSpeed* Time.deltaTime;
+        transform.position += moveVelocity;
+    }
+
+    private void TryRoll()
+    {
+
     }
 }
