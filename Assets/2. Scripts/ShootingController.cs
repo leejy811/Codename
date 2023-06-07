bd437f36676adfc61dcca83606f15d86fb14ce6d
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-06-07 21:04:55 +0900

    Revert "Restrict Player movement by its states"
    This reverts commit a4d022935c386162bb2ae6843100ca98aca8ce8b.

--- Assets/2. Scripts/ShootingController.cs
+++ Assets/2. Scripts/ShootingController.cs
@@ -5,7 +5,6 @@ using DG.Tweening;
 
 public class ShootingController : MonoBehaviour
 {
-    #region Member Var
     public enum PlayerStates
     {
         Idle,
@@ -16,13 +15,12 @@ public class ShootingController : MonoBehaviour
     public PlayerStates PlayerState { get; private set; } = PlayerStates.Idle;
 
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float rollDistance;
 
     private float moveX;
     private float moveY;
     private bool isDead = false;
     private bool isRolling = false;
-    #endregion
+
 
     void Start()
     {
@@ -34,9 +32,7 @@ public class ShootingController : MonoBehaviour
         if (isDead)
             return;
 
-        moveX = Input.GetAxisRaw("Horizontal");
-        moveY = Input.GetAxisRaw("Vertical");
-
+        PlayerState = PlayerStates.Idle;
         TryMove();
         TryRoll();
 
@@ -49,22 +45,19 @@ public class ShootingController : MonoBehaviour
                 break;
             case PlayerStates.Dead:
                 break;
-            default:
-                break;
         }
     }
 
     private void TryMove()
     {
-        if (moveX == 0 && moveY == 0)
-        {
-            PlayerState = PlayerStates.Idle;
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
             return;
-        }
         if (isRolling)
             return;
 
         PlayerState = PlayerStates.Move;
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
     }
 
     private void Move()
@@ -75,7 +68,7 @@ public class ShootingController : MonoBehaviour
 
     private void TryRoll()
     {
-        if (!Input.GetKeyDown(KeyCode.Space) || isRolling)
+        if (!Input.GetKeyDown(KeyCode.Space))
             return;
         PlayerState = PlayerStates.Roll;
         isRolling = true;
@@ -84,10 +77,9 @@ public class ShootingController : MonoBehaviour
 
     IEnumerator Roll()
     {
-        var roll = transform.DOMove(new Vector3(moveX, moveY, 0).normalized*rollDistance, 0.5f).SetRelative().SetEase(Ease.OutQuad);
+        var roll = transform.DOMove(new Vector3(1, 1, 0), 1f).SetRelative();
         yield return roll.WaitForCompletion();
 
-        PlayerState = PlayerStates.Idle;
         isRolling = false;
     }
 }
