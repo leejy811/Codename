930abfd30e7da00c3aee32644496ea14a8873164
MK <simon365@naver.com>
2023-06-14 23:56:45 +0900

    [Structure] Monster Init
    using Inheritence

--- Assets/2. Scripts/MonsterController.cs
+++ Assets/2. Scripts/MonsterController.cs
@@ -7,11 +7,25 @@ public enum MonsterType
     temp,
 }
 
-
 public abstract class MonsterController : MonoBehaviour
 {
     [SerializeField] protected string monsterName;
     [SerializeField] protected float monsterHp;
     [SerializeField] protected float monsterSpeed;
     [SerializeField] protected MonsterType monsterType;
+
+    protected void Init()
+    {
+
+    }
+
+    public void GetDamage()
+    {
+
+    }
+
+    public virtual void Attack()
+    {
+
+    }
 }
---
+++ Assets/2. Scripts/MonsterTemp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterTemp : MonsterController
+{
+    void Start()
+    {
+        Init();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+
+    }
+}
