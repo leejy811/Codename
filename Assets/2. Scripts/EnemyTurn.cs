748c0fd21bfd0c5734d0a87c9313c1f9924d441b
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-06-17 20:23:25 +0900

    Feat : EnemyC 타입 추가
    .

--- Assets/2. Scripts/EnemyTurn.cs
+++ Assets/2. Scripts/EnemyTurn.cs
@@ -31,6 +31,8 @@ partial class EnemyTurn : MonoBehaviour
     [SerializeField]
     public List<Vector2> enemyMovePos;
 
+    // 이동할 방향
+    Vector3 nextMovePos;
 
     private void Start()
     {
@@ -66,6 +68,12 @@ partial class EnemyTurn : MonoBehaviour
         {
             StartCoroutine(enemyA_move());
         }
+
+        // Enemy C
+        if (enemyType == EnemyType.enemyC)
+        {
+            enemyC_move();
+        }
     }
 
 
@@ -83,7 +91,7 @@ partial class EnemyTurn
         if (circularSector.GetComponent<CircularSector>().isCollision == true) yield return null;
         else
         {
-            Vector3 nextMovePos = enemyMovePos[(curPosIdx++) % enemyMovePos.Count];
+            nextMovePos = enemyMovePos[(curPosIdx++) % enemyMovePos.Count];
             float duration = Vector2.Distance(transform.position, nextMovePos) / velocity;
 
             // 부채꼴 크기의 탐색 범위 회전
@@ -101,5 +109,79 @@ partial class EnemyTurn
 
             StartCoroutine(enemyA_move());
         }
+    }
+
+
+    private void enemyC_move(bool isUserTurn=true)
+    {
+
+        // 유저 턴인 경우 이동할 경로 표시
+        if (isUserTurn)
+        {
+            int Rand_X = Random.Range((int)this.transform.localPosition.x-3, (int)this.transform.localPosition.x+3);
+            int Rand_Y = Random.Range((int)this.transform.localPosition.y-3, (int)this.transform.localPosition.y+3);
+            nextMovePos = new Vector2(Rand_X, Rand_Y);
+
+            GenerateRoad();
+        }
+        // 상대 턴인 경우 이동
+        else
+        {
+            float duration = Vector2.Distance(transform.position, nextMovePos) / velocity;
+
+            // 부채꼴 크기의 탐색 범위 회전
+            Vector3 _dir = nextMovePos - transform.position;
+            circularSector.transform.DORotate(new Vector3(0, 0, (Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg)), .3f); // circularSector.transform.rotation = Quaternion.Euler(new Vector3(0, 0, (Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg)));// = Quaternion.Euler(_dir);
+                                                                                                                      //Debug.Log(Mathf.Atan2(_dir.y, _dir.x)*Mathf.Rad2Deg+180);
+
+            newEnemyRoad.SetActive(false);
+            tweener = transform.DOMove(nextMovePos, duration).SetEase(Ease.Linear)
+                .OnComplete(() => { 
+                    enemyC_move(true);
+                    newEnemyRoad.SetActive(true);
+                }); // 이동 끝나면 유저턴으로 패스
+        }
+    }
+
+    public void TurnPass()
+    {
+        enemyC_move(false);
+    }
+}
+
+// 적 이동경로 표시
+partial class EnemyTurn
+{
+    [SerializeField] GameObject roadPrefab;
+    private GameObject newEnemyRoad;
+
+    private void GenerateRoad()
+    {
+        newEnemyRoad = Instantiate(roadPrefab,this.transform);
+        newEnemyRoad.transform.localPosition = new Vector3(0,0,0);
+
+        GameObject _road = roadPrefab.transform.GetChild(0).GetChild(0).gameObject;
+        GameObject _point = roadPrefab.transform.GetChild(0).GetChild(1).gameObject;
+
+        // 시작점 및 끝점
+        GameObject startPoint = Instantiate(_point, newEnemyRoad.transform);
+        GameObject endPoint = Instantiate(_point, newEnemyRoad.transform);
+        startPoint.SetActive(true);
+        endPoint.SetActive(true);
+        endPoint.transform.position = nextMovePos;
+
+
+        // 길 경로 및 위치 설정
+        float dist = Vector2.Distance(startPoint.transform.localPosition, endPoint.transform.localPosition);
+        for (int i = 1; i <= (int)dist; i++)
+        {
+            GameObject road = Instantiate(_road, newEnemyRoad.transform.GetChild(0));
+            road.SetActive(true);
+            road.transform.localPosition = new Vector2(startPoint.transform.localPosition.x + i, 0);
+            //float dist = Vector2.Distance(startPoint.transform.position, endPoint.transform.position);
+        }
+        float angle = Mathf.Atan2(endPoint.transform.localPosition.y, endPoint.transform.localPosition.x) * Mathf.Rad2Deg;
+        newEnemyRoad.transform.GetChild(0).DORotate(new Vector3(0, 0, angle),0f);
+
     }
 }
