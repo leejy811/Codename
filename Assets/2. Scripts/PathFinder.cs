0be63ea4542c50a9875bb9b136d5f6af10ad33ba
MK <simon365@naver.com>
2023-06-19 16:54:03 +0900

    [Func] GetDir
    + static func
    returns the direction of the shortest route

--- Assets/2. Scripts/PathFinder.cs
+++ Assets/2. Scripts/PathFinder.cs
@@ -2,21 +2,49 @@ using System.Collections.Generic;
 
 public class PathFinder 
 {
-    public static int[,] dirs = { { -1, -1 },  { -1, 0 }, { -1, 1 }, { 0, 1 },  { 1, 1 }, { 1, 0 }, { 1, -1 },  { 0, -1 } };
+    // move to 8-directions
+    public  readonly static int[,] dirs = { { -1, -1 },  { -1, 0 }, { -1, 1 }, { 0, 1 },  { 1, 1 }, { 1, 0 }, { 1, -1 },  { 0, -1 } };
     
+    // returns the first step of the "Best Route" 
+    // using BFS, generic queue
     public static int[] GetDir(int[,] map,int[] playerPos,int[] currentPos,int sizeX,int sizeY)
     {
         Queue<(int, int, int)> queue = new Queue<(int, int, int)>();
         int[,] visited = new int[map.GetLength(0),map.GetLength(1)];
+        int xGap = sizeX % 2;
+        int yGap = sizeY % 2;
+
+        // Start from the initial monster posiiton
         queue.Enqueue((currentPos[0], currentPos[1], -1));
+        visited[currentPos[0], currentPos[1]] = 1;
 
         while (queue.Count > 0)
         {
             var temp = queue.Dequeue();
-            
+
+            // return the 'first' direction of the route. monster will move to the direction and calc the best route again.
+            if (temp.Item1 == playerPos[0] && temp.Item2 == playerPos[1])
+                return new int[] { dirs[temp.Item3, 0], dirs[temp.Item3, 1] };          
+
+            // check for all available tiles and put into the queue
+            for (int i = 0; i < dirs.GetLength(0); i++)
+            {
+                int dx = temp.Item1 + dirs[i, 0];
+                int dy = temp.Item2 + dirs[i, 1];
+
+                if (dx < 0 || dy < 0 || dx >= map.GetLength(0) || dy >= map.GetLength(1))
+                    continue;   // pass if outside of the map
+                if (visited[dx, dy] == 1 || map[dx,dy]==1)
+                    continue;   // pass if already visited
+
+                if (temp.Item3 == -1)   // set the initial direction when calculating the first tile
+                    queue.Enqueue((dx, dy, i)); 
+                else                             // maintain the first direction of the route until reaching the player
+                    queue.Enqueue((dx, dy, temp.Item3));
+                visited[dx, dy] = 1;
+            }
         }
 
-
-        return new int[]{ -1,-1};
+        return new int[]{ -1,-1};   // return error when the monster cannot reach the player
     }
 }
