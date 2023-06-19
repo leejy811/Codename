d05c9f09706a4d0a66f66d1abb1bdcef5ea5e3fb
MK <simon365@naver.com>
2023-06-19 23:12:32 +0900

    [Test] Monster movement test
    will be fixed
    + include monster size when calculating the shortest path
    + change into A* algorithm to add different weight to each directions

--- Assets/2. Scripts/MonsterController.cs
+++ Assets/2. Scripts/MonsterController.cs
@@ -7,12 +7,28 @@ public enum MonsterType
     temp,
 }
 
+public enum MonsterState
+{
+    Idle,
+    Move,
+    Attack,
+}
+
 public abstract class MonsterController : MonoBehaviour
 {
     [SerializeField] protected string monsterName;
     [SerializeField] protected float monsterHp;
     [SerializeField] protected float monsterSpeed;
+    [SerializeField] protected float monsterDamage;
     [SerializeField] protected MonsterType monsterType;
+
+    protected bool isMoving = false;
+    protected bool isAttacking = false;
+
+    protected MonsterState monsterState=MonsterState.Idle;
+
+    protected int posX;
+    protected int posY;
 
     protected void Init()
     {
--- Assets/2. Scripts/MonsterTemp.cs
+++ Assets/2. Scripts/MonsterTemp.cs
@@ -1,17 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MonsterTemp : MonsterController
 {
+    int[,] map = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+        };
+    int x = 4;
+    int y = 8;
+    bool ismoving=false;
     void Start()
     {
         Init();
+        
     }
 
     public override void Attack()
     {
         base.Attack();
 
+    }
+
+    private void Update()
+    {
+        TryMove();
+    }
+
+    private void TryMove()
+    {
+        if (ismoving)
+            return;
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        ismoving = true;
+        var d= PathFinder.GetDir(map, new int[] { 2, 2 }, new int[] { x, y }, 1, 1);
+        var tween = transform.DOMove(new Vector3(d[1], -d[0], 0), 1f).SetRelative().SetEase(Ease.Linear);
+        x += d[0];
+        y += d[1];
+        Debug.Log(x + "," + y);
+        yield return tween.WaitForCompletion();
+        ismoving = false;
     }
 }
