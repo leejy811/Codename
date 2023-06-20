42a0d90d5e3c1fb8f550781be4a64677700fe915
MK <simon365@naver.com>
2023-06-21 03:22:09 +0900

    [Func] Make Stage manger
    + manage map info
    + calc player pos(world pos) to grid coordinate system

---
+++ Assets/2. Scripts/StageManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public static StageManager instance;
+    
+
+    [SerializeField] PlayerController player;
+
+    private int mapX;
+    private int mapY;
+    public int[,] map = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+        };
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        Init();
+    }
+
+    private void Init()
+    {
+        mapX = map.GetLength(1);
+        mapY = map.GetLength(0);
+    }
+
+    public int[] PlayerPos()
+    {
+        return new int[]{ (int)((mapY / 2) - player.transform.position.y), (int)(player.transform.position.x + (mapX / 2)) };
+    }
+}
