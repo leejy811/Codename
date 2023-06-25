b48442c3246a634021c2fe5406b5c695e6ee8af0
MK <simon365@naver.com>
2023-06-25 12:15:18 +0900

    [Fix] Assign move func to the parrent class

--- Assets/2. Scripts/MonsterController.cs
+++ Assets/2. Scripts/MonsterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public enum MonsterType
 {
@@ -40,9 +41,23 @@ public abstract class MonsterController : MonoBehaviour
 
     }
 
-    public void MoveToPlayer()
+    protected void TryMove()
     {
+        if (isMoving)
+            return;
+        StartCoroutine(Move());
+    }
 
+    IEnumerator Move()
+    {
+        isMoving = true;
+        var d = PathFinder.GetDir(new int[] { posX, posY }, 3, 3);
+        var tween = transform.DOMove(new Vector3(d[1], -d[0], 0), 1f).SetRelative().SetEase(Ease.Linear);
+        posX += d[0];
+        posY += d[1];
+
+        yield return tween.WaitForCompletion();
+        isMoving = false;
     }
 
     public virtual void Attack()
--- Assets/2. Scripts/MonsterTemp.cs
+++ Assets/2. Scripts/MonsterTemp.cs
@@ -5,10 +5,6 @@ using DG.Tweening;
 
 public class MonsterTemp : MonsterController
 {
-    
-    int x = 4;
-    int y = 8;
-    bool ismoving=false;
     void Start()
     {
         Init();
@@ -24,24 +20,5 @@ public class MonsterTemp : MonsterController
     private void Update()
     {
         TryMove();
-    }
-
-    private void TryMove()
-    {
-        if (ismoving)
-            return;
-        StartCoroutine(Move());
-    }
-
-    IEnumerator Move()
-    {
-        ismoving = true;
-        var d= PathFinder.GetDir(new int[] { x, y }, 3, 3);
-        var tween = transform.DOMove(new Vector3(d[1], -d[0], 0), 1f).SetRelative().SetEase(Ease.Linear);
-        x += d[0];
-        y += d[1];
-        
-        yield return tween.WaitForCompletion();
-        ismoving = false;
     }
 }
