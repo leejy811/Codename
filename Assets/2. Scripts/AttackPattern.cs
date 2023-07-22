977816d717eceb5b8355e4b9161fdb40ba32add3
MK <simon365@naver.com>
2023-07-22 11:06:40 +0900

    Apply attack pattern

--- Assets/2. Scripts/AttackPattern.cs
+++ Assets/2. Scripts/AttackPattern.cs
@@ -5,15 +5,42 @@ using DG.Tweening;
 
 public class AttackPattern : MonoBehaviour
 {
-    public static IEnumerator PatternA(GameObject bullet, int bulletCount, float bulletSpeed,float damage, Vector3 originPos)
+    public static readonly int patternCount = 1; 
+
+    /// <summary>
+    /// Enemy Attack Pattern, make number of bullets sequentially and shoot
+    /// </summary>
+    /// <param name="bullet">bullet prefab</param>
+    /// <param name="bulletCount">the number of bullet to be used</param>
+    /// <param name="bulletSpeed">speed of bullet</param>
+    /// <param name="damage">damage of bullet</param>
+    /// <param name="originPos">base position to be used for bullet generation. give the 'enemy position' in general</param>
+    /// <param name="monster">the object who starts attack</param>
+    /// <returns></returns>
+    public static IEnumerator PatternA(MonsterBulletController bullet, int bulletCount, float bulletSpeed,float damage, Vector3 originPos, MonsterController monster)
     {
+        var bullets = new List<MonsterBulletController>();
+
         for(int theta=0; theta < 360; theta += (360/bulletCount))
         {
-            Vector2 bulletPos = new (originPos.x + Mathf.Cos(theta)*3, originPos.y + Mathf.Sin(theta)*3);
-            var b = Instantiate(bullet, new Vector3(bulletPos.x, bulletPos.y, 0), Quaternion.identity);
+            // set bullet pos to draw a circle from the monster
+            Vector2 bulletPos = new (originPos.x + Mathf.Cos(theta*Mathf.Deg2Rad)*1.5f, originPos.y + Mathf.Sin(theta * Mathf.Deg2Rad) *1.5f);
+            bullets.Add(Instantiate(bullet, new Vector3(bulletPos.x, bulletPos.y, 0), Quaternion.identity));
             yield return new WaitForSeconds(0.5f);
         }
 
-        yield return null;
+        //shoot all bullets at once after all bullets generated
+        foreach (var b in bullets)
+        {
+            try
+            {
+                b.Shoot();
+            }
+            catch { Debug.Log("bullet does not exist"); }
+        }
+
+        yield return new WaitForSeconds(1);
+
+        monster.FinishAttack();
     }
 }
