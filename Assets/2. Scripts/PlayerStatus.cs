10f8939440f2cad1154856ae34631a8cd7261281
leejy811 <lee38956162@gmail.com>
2023-09-20 19:34:14 +0900

    init Player Status & make Item File

--- Assets/2. Scripts/PlayerStatus.cs
+++ Assets/2. Scripts/PlayerStatus.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using MoreMountains.InventoryEngine;
 
-public class PlayerStatus : MonoBehaviour
+public class PlayerStatus : MonoBehaviour, MMEventListener<MMInventoryEvent>
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterMovement movement;
+    CharacterInventory inventory;
+
+    private void Start()
     {
-        
+        movement = gameObject.GetComponent<CharacterMovement>();
+        inventory = gameObject.GetComponent<CharacterInventory>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ApplyZigzagItem(bool enable)
     {
-        
+        movement.MovementSpeed *= enable ? -2f : -0.5f;
     }
+
+    public void OnMMEvent(MMInventoryEvent inventoryEvent)
+    {
+		if (inventoryEvent.InventoryEventType == MMInventoryEventType.Pick)
+		{
+            switch (inventoryEvent.EventItem.ItemID)
+            {
+                case "Zigzag":
+                    ApplyZigzagItem(true);
+                    break;
+            }
+		}
+        else if (inventoryEvent.InventoryEventType == MMInventoryEventType.Drop)
+        {
+            switch (inventoryEvent.EventItem.ItemID)
+            {
+                case "Zigzag":
+                    ApplyZigzagItem(true);
+                    break;
+            }
+        }
+    }
+
+	private void OnEnable()
+	{
+		this.MMEventStartListening<MMInventoryEvent>();
+	}
+
+	private void OnDisable()
+	{
+		this.MMEventStopListening<MMInventoryEvent>();
+	}
 }
