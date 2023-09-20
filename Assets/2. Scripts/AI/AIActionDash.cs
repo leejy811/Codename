26727cd5b76b3cf9092ec6858e0e5a6d5ea27b0e
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-09-20 22:12:56 +0900

    Add Boss Dash Scripts

---
+++ Assets/2. Scripts/AI/AIActionDash.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+
+namespace MoreMountains.TopDownEngine
+{
+    public class AIActionDash : AIAction
+    {
+		public override void PerformAction()
+		{
+			
+		}
+
+
+	}
+}
+
