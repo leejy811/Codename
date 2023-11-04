a07fdab8022088c7fc349bc42732075b145d5a4e
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-11-04 14:37:41 +0900

    Boss Facing Update

--- Assets/2. Scripts/BossBlend.cs
+++ Assets/2. Scripts/BossBlend.cs
@@ -7,16 +7,36 @@ public class BossBlend : MonoBehaviour
 {
     private CharacterHandleWeapon characterHandle;
     private Character character;
+    private CharacterMovement characterMovement;
+
+    [SerializeField]
+    private GameObject characterModel;
+    Vector2 characterOrient2D;
 
     private void Start()
     {
         characterHandle = gameObject.GetComponent<CharacterHandleWeapon>();
         character = gameObject.GetComponent<Character>();
+        characterMovement = gameObject.GetComponent<CharacterMovement>();
+
     }
 
     private void Update()
     {
-        character._animator.SetFloat("xDir", characterHandle.WeaponAimComponent.CurrentAim.x);
-        character._animator.SetFloat("yDir", characterHandle.WeaponAimComponent.CurrentAim.y);
+        if (characterHandle == null || character == null || characterMovement == null)
+            return;
+        if (characterHandle.WeaponAimComponent == null)
+            characterOrient2D = new Vector2(1, 1);
+        else
+        {
+            characterOrient2D = new Vector2(characterHandle.WeaponAimComponent.CurrentAim.x, characterHandle.WeaponAimComponent.CurrentAim.y);
+        }
+        if (characterOrient2D.x < 0)
+            characterModel.transform.localScale = new Vector3(-1, 1, 1);
+        else
+            characterModel.transform.localScale = new Vector3(1, 1, 1);
+
+        character._animator.SetFloat("xDir", characterOrient2D.x);
+        character._animator.SetFloat("yDir", characterOrient2D.y);
     }
 }
